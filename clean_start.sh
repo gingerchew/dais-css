f11da104222b8d5c4bf6e4002258f4c9cc608501
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-01-16 22:59:45 -0500

    updates scripts
    after going through everything again, I cleaned out all the old
    scripts that were no longer serving a purpose
    
    also added a clean start script
    this removes all the files that are 'processing'
    and cleans out the node modules
    then it removes the built files, e.g. dais.css
    
    also reserved some of the scripts I removed in case they
    become useful for debugging later on

---
+++ clean_start.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+REMOVE=(
+    processing
+    node_modules
+    dais.css
+    dais.min.css
+    dais.min.css.map
+)
+# rm -rf ./processing
+# rm -rf ./node_modules
+# rm dais.css
+# rm dais.min.css
+# rm dais.min.css.map
+
+for i in ${REMOVE[@]}; do
+    if [[ -f $i ]]; then
+        rm $i
+        continue
+    fi
+    if [[ -d $i ]]; then
+        rm -rf $i
+        continue
+    fi
+done
+#! figure out a way to re install based on package manager npm/yarn/pnpm
+exit 0
