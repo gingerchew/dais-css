a6e3b450ab0057d21e4b838c071138b4807e6b4c
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-01-16 20:52:36 -0500

    moves css imports task
    now there is more room for more growth in the future
    
    in case of expansion outside of a minimal stylesheet

---
+++ tasks/gulp.css.js
@@ -0,0 +1,29 @@
+const { src, dest } = require('gulp');
+
+const rename = require('gulp-rename');
+const cssimport = require('gulp-cssimport');
+
+const { dest: { dev, prod }, start } = paths = require('./gulp.paths');
+const { env } = require('./gulp.utils');
+
+//* Handle imports/Concatenate Files
+function imports() {
+    const cssimport_options = {
+        includePaths: [start]
+    };
+
+    if (env === 'dev') {
+        return src(start)
+            .pipe(cssimport(cssimport_options))
+            .pipe(rename('main.import.css'))
+            .pipe(dest(dev));
+    }
+    return src(start)
+        .pipe(cssimport(cssimport_options))
+        .pipe(rename('dais.css'))
+        .pipe(dest(prod));
+}
+
+module.exports = {
+    imports,
+}
