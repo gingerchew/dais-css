7b9e70bc65c0ea5f5ea416a33fe00953b80d116e
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-01-16 20:53:43 -0500

    moves postcss out on its own
    now theres just a little bit less code to search through

---
+++ tasks/gulp.postcss.js
@@ -0,0 +1,68 @@
+const { src, dest } = require('gulp');
+
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const rename = require('gulp-rename');
+
+const { dest: { dev, prod } } = paths = require('./gulp.paths');
+const { clean, env } = require('./gulp.utils');
+
+const plugins = {
+    process: [
+        require('postcss-each'),
+        require('postcss-responsive-type'),
+        require('postcss-custom-media'),
+        require('postcss-easing-gradients'),
+        require('postcss-combine-media-query')
+    ],
+    preset: [
+        require('postcss-preset-env')({
+            config: {
+                stage: 3,
+                browsers: '',
+                preserve: true
+            }
+        })
+    ],
+    min: [
+        require('cssnano')
+    ]
+}
+
+function processes() {
+    if (env === 'dev') {
+        return src('./processing/main.import.css')
+            .pipe(postcss(plugins.process))
+            .pipe(rename('main.processed.css'))
+            .pipe(dest(dev))
+            .pipe(rename('dais.css'))
+            .pipe(dest(prod));
+    }
+    return src('dais.css')
+        .pipe(postcss(plugins.process))
+        .pipe(dest(prod))
+
+}
+
+/* Internal functions */
+function preset() {
+    return src('dais.css')
+        .pipe(postcss(plugins.preset))
+        .pipe(dest(prod))
+}
+
+
+function minify() {
+    return src('./dais.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss(plugins.min))
+        .pipe(rename('dais.min.css'))
+        .pipe(sourcemaps.write(prod))
+        .pipe(dest(prod));
+}
+
+module.exports = {
+    minify,
+    preset,
+    processes,
+};
