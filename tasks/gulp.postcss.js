260db355a7b58a30c098e7d90fe23aea8ceafa4d
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-02-17 22:51:19 -0500

    fixed bug //* * ! Error - Message: Cannot read property 'root' of undefined * * fileName: /Users/gngrchw/Code/dais-css/dais.css *   domainEmitter: [object Object] *   domainThrown: false */
    forgot to wrap postcss plugin in an array

--- tasks/gulp.postcss.js
+++ tasks/gulp.postcss.js
@@ -10,86 +10,75 @@ const mediaQueriesPost = require('postcss-combine-media-query');
 const presetPost = require('postcss-preset-env');
 const nanoPost = require('cssnano');
 
-const { dest: { dev, prod } } = paths = require('./gulp.paths');
+const {
+	dest: { dev, prod }
+} = (paths = require('./gulp.paths'));
 
 const plugins = {
-    process: [
-        eachPost(),
-        responsiveTypePost(),
-        mediaQueriesPost()
-    ],
-    preset: {
-        config: {
-            stage: 1,
-            browsers: '',
-            preserve: true,
-        }
-    },
-    min: [
-        nanoPost()
-    ]
+	process: [eachPost(), responsiveTypePost(), mediaQueriesPost()],
+	preset: {
+		config: {
+			stage: 1,
+			browsers: '',
+			preserve: true
+		}
+	},
+	min: [nanoPost()]
 };
 
 function each() {
-    return src('./processing/main.import.css')
-        .pipe(postcss([eachPost()]))
-        .pipe(rename('main.each.css'))
-        .pipe(dest(dev));
+	return src('./processing/main.import.css')
+		.pipe(postcss([eachPost()]))
+		.pipe(rename('main.each.css'))
+		.pipe(dest(dev));
 }
 
 function responsiveType() {
-    return src('./processing/main.each.css')
-        .pipe(postcss([responsiveTypePost()]))
-        .pipe(rename('main.type.css'))
-        .pipe(dest(dev));
+	return src('./processing/main.each.css')
+		.pipe(postcss([responsiveTypePost()]))
+		.pipe(rename('main.type.css'))
+		.pipe(dest(dev));
 }
 
 function mediaQueries() {
-    return src('./processing/main.type.css')
-        .pipe(postcss([mediaQueriesPost()]))
-        .pipe(rename('main.media.css'))
-        .pipe(dest(dev));
+	return src('./processing/main.type.css')
+		.pipe(postcss([mediaQueriesPost()]))
+		.pipe(rename('main.media.css'))
+		.pipe(dest(dev));
 }
 
 function processed() {
-    return src('./processing/main.media.css')
-        .pipe(rename('main.processed.css'))
-        .pipe(dest(dev))
-        .pipe(rename('dais.css'))
-        .pipe(dest(prod));
+	return src('./processing/main.media.css')
+		.pipe(rename('main.processed.css'))
+		.pipe(dest(dev))
+		.pipe(rename('dais.css'))
+		.pipe(dest(prod));
 }
 
 /* Internal functions */
 function preset() {
-    return src('./dais.css')
-        /*
-         * ! Error - Message: Cannot read property 'root' of undefined
-         * 
-         * fileName: /Users/gngrchw/Code/dais-css/dais.css
-         *   domainEmitter: [object Object]
-         *   domainThrown: false
-         */
-        .pipe(postcss(presetPost(plugins.preset)))
-        .pipe(dest(prod))
+	return src('./dais.css')
+		.pipe(postcss([presetPost(plugins.preset)]))
+		.pipe(dest(prod));
 }
 preset.description = `Applies the preset-env from postcss to make the css more compatible`;
 
 function minify() {
-    return src('./dais.css')
-        .pipe(sourcemaps.init())
-        .pipe(postcss(plugins.min))
-        .pipe(rename('dais.min.css'))
-        .pipe(sourcemaps.write(prod))
-        .pipe(dest(prod));
+	return src('./dais.css')
+		.pipe(sourcemaps.init())
+		.pipe(postcss(plugins.min))
+		.pipe(rename('dais.min.css'))
+		.pipe(sourcemaps.write(prod))
+		.pipe(dest(prod));
 }
 minify.description = `Minifies CSS and produces a sourcemap`;
 
 module.exports = {
-    minify,
-    preset,
-    processed,
-    processes: series(each, responsiveType, mediaQueries, processed),
-    each,
-    responsiveType,
-    mediaQueries
+	minify,
+	preset,
+	processed,
+	processes: series(each, responsiveType, mediaQueries, processed),
+	each,
+	responsiveType,
+	mediaQueries
 };
