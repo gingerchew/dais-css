421b0cb691dc138cf6af3da5bea0e57d9b3c28b6
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-02-20 22:46:02 -0500

    un did the series in the prod task for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 const { series, parallel } = require('gulp');
 
-const { buildCSS, devCSS, cleanOutputs, serve, observe } = require('./tasks');
-
-exports.cleanOutputs = cleanOutputs;
+const { buildCSS, devCSS, serve, observe } = require('./tasks');
 
 exports.default = parallel(series(cleanOutputs, devCSS), series(serve, observe));
 /* task.production: task runs and builds everything properly, but doesn't ever terminate back to the terminal */
-exports.production = series(cleanOutputs, buildCSS);
+exports.production = buildCSS;
