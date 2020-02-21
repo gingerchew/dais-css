cfe74d2adc9d72fdab544e6445c61bbf6ba86edb
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-02-21 00:26:06 -0500

    adds not to remove setTimeout, need to add check for production/default

--- gulpfile.js
+++ gulpfile.js
@@ -7,5 +7,7 @@ const process = require('process');
 exports.default = parallel(devCSS, series(serve, observe));
 
 exports.production = series(buildCSS);
-
+// ** Add in .env support so that we can avoid running this function on default
+/*
 setTimeout(() => process.exit(0), 2000);
+*/
