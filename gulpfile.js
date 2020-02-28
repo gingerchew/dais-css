d0e0e00fa8da906bdce23fb62f4b296fb96bea09
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-02-27 22:31:37 -0500

    finished build process to get production setup right

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,8 @@
+// const prod = process.argv.length !== 0 && process.argv.indexOf('--prod') !== -1 && true;
+const processMod = require('process');
+
+const prod = processMod.argv !== undefined && processMod.argv.indexOf('--prod') !== -1 && true;
+
 const { series, parallel } = require('gulp');
 
 const { buildCSS, devCSS, serve, observe } = require('./tasks');
@@ -7,6 +12,7 @@ const process = require('process');
 exports.default = parallel(devCSS, series(serve, observe));
 
 exports.production = series(buildCSS);
+if (prod) setTimeout(() => process.exit(0), 2000);
 // ** Add in .env support so that we can avoid running this function on default
 /*
 setTimeout(() => process.exit(0), 2000);
